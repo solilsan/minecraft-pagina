6687cd44f336e8852b2a1978b226ff87fe544517
solilsan <alexsandrox1000x@gmail.com>
2019-02-16 16:34:20 +0100

    registro

--- view/js/index.js
+++ view/js/index.js
@@ -1,18 +1,20 @@
 
 $(document).ready(function(){
 
+    $("#login").fadeIn("slow");
+
     $("#abrirRegistro").click(function(){
         $("#login").fadeOut("slow");
         setTimeout(function(){
             $("#registro").fadeIn("slow");
-        }, 3000);
+        }, 1000);
     });
 
     $("#volverLogin").click(function(){
         $("#registro").fadeOut("slow");
         setTimeout(function(){
             $("#login").fadeIn("slow");
-        }, 3000);
+        }, 1000);
     });
 
 });
